360e7973bfa79533c69d703d2d4c6a9d4e45f560
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-08-24 23:07:34 +0530

    switch case method

--- Stack/Lecture-1/Validparanthesis.java
+++ Stack/Lecture-1/Validparanthesis.java
@@ -25,3 +25,33 @@ class Solution {
 
     }
 }
+
+
+
+// another way by using switch case
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        
+       for (char ch : s.toCharArray()) {
+            switch (ch) {
+                case '(':
+                case '{':
+                case '[':
+                    st.push(ch);
+                    break;
+                case ')':
+                    if (st.isEmpty() || st.pop() != '(') return false;
+                    break;
+                case '}':
+                    if (st.isEmpty() || st.pop() != '{') return false;
+                    break;
+                case ']':
+                    if (st.isEmpty() || st.pop() != '[') return false;
+                    break;
+            }
+        }
+        return st.isEmpty();
+        
+    }
+}
