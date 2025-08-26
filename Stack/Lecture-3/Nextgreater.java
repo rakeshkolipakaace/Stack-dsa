9fba3859a6231e201f5e317737578c4cab10bdf0
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-08-26 15:56:28 +0530

    next greater

---
+++ Stack/Lecture-3/Nextgreater.java
@@ -0,0 +1,26 @@
+// optimal solution
+class Solution {
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+        // code here
+        int n=arr.length;
+        ArrayList<Integer>res=new ArrayList<>();
+        Stack<Integer>st=new Stack<>();
+        for(int i=0;i<n;i++){
+            res.add(-1);
+        }
+        
+        for(int i=n-1;i>=0;i--){
+            while(st.size()>0&&st.peek()<=arr[i]){
+                st.pop();
+                
+            }
+            if(!st.isEmpty()){
+                  res.set(i,st.peek());
+
+                
+            }
+            st.push(arr[i]);
+        }
+        return res;
+    }
+}
