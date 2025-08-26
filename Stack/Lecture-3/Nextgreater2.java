e2651b94211254e6979825e689b1df85bbc467f5
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-08-26 17:08:24 +0530

    next greater 2 optimal

--- Stack/Lecture-3/Nextgreater2.java
+++ Stack/Lecture-3/Nextgreater2.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
 
-// brute force
-         int n = nums.length;
+        // brute force
+        int n = nums.length;
         int ans[] = new int[n];
-        Arrays.fill(ans,-1);
+        Arrays.fill(ans, -1);
 
-        for (int i = 0; i < n; i++) { 
+        for (int i = 0; i < n; i++) {
             for (int j = 1; j < n; j++) {
                 int nextIndex = (i + j) % n;
                 if (nums[nextIndex] > nums[i]) {
@@ -17,9 +17,21 @@ class Solution {
         }
         return ans;
 
+        // optimal
+        int n = nums.length;
+        int ans[] = new int[n];
+        Arrays.fill(ans, -1);
+        Stack<Integer> st = new Stack<>();
+        st.push(0);
 
-     
+        for (int i = 1; i < 2 * n; i++) {
+            while (!st.isEmpty() && nums[st.peek()] < nums[i % n]) {
+                ans[st.pop()] = nums[i % n];
+            }
+            st.push(i % n);
 
-        
+        }
+        return ans;
+
     }
 }
