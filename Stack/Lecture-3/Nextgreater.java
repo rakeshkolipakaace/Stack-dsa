ab703825c67ca37f9fcf1deed5f41c5f86ab3047
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-08-26 15:58:02 +0530

    brute force

--- Stack/Lecture-3/Nextgreater.java
+++ Stack/Lecture-3/Nextgreater.java
@@ -1,23 +1,48 @@
+// brute force
+
+class Solution {
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+        // code here
+        int n = arr.length;
+        ArrayList<Integer> res = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            res.add(-1);
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] > arr[i]) {
+                    res.set(i, arr[j]);
+                    break;
+                }
+            }
+        }
+
+        return res;
+    }
+}
+
 // optimal solution
 class Solution {
     public ArrayList<Integer> nextLargerElement(int[] arr) {
         // code here
-        int n=arr.length;
-        ArrayList<Integer>res=new ArrayList<>();
-        Stack<Integer>st=new Stack<>();
-        for(int i=0;i<n;i++){
+        int n = arr.length;
+        ArrayList<Integer> res = new ArrayList<>();
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
             res.add(-1);
         }
-        
-        for(int i=n-1;i>=0;i--){
-            while(st.size()>0&&st.peek()<=arr[i]){
-                st.pop();
-                
-            }
-            if(!st.isEmpty()){
-                  res.set(i,st.peek());
 
-                
+        for (int i = n - 1; i >= 0; i--) {
+            while (st.size() > 0 && st.peek() <= arr[i]) {
+                st.pop();
+
+            }
+            if (!st.isEmpty()) {
+                res.set(i, st.peek());
+
             }
             st.push(arr[i]);
         }
