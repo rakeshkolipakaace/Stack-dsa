bcb0cb439b6bb747407b9881bf19c4088c8e1773
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-08-26 18:15:45 +0530

    next smallest

---
+++ Stack/Lecture-3/Nextsmallest.java
@@ -0,0 +1,11 @@
+// User function Template for Java
+
+class Solution {
+    public void immediateSmaller(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            arr[i] = (arr[i + 1] < arr[i]) ? arr[i + 1] : -1;
+        }
+        arr[n - 1] = -1;
+    }
+}
